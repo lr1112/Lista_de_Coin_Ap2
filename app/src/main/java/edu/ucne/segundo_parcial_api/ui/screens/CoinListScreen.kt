3e7b6dc79c3f59466fa40e7d5a0d95352669ff5a
lr1112 <luis_rosario_0324@hotmail.com>
2022-08-04 11:30:43 -0400

    creando la consulta de coin

---
+++ app/src/main/java/edu/ucne/segundo_parcial_api/ui/screens/CoinListScreen.kt
@@ -0,0 +1,67 @@
+package edu.ucne.segundo_parcial_api.ui.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import edu.ucne.segundo_parcial_api.ui.screens.componentes.CoinItem
+
+
+@Composable
+fun CoinListScreen(
+    viewModel: CoinViewModel = hiltViewModel(),
+    navHostController: NavHostController,
+) {
+    val state = viewModel.state.value
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Consulta de Criptomonedas",
+                        fontFamily = FontFamily.Serif,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { navHostController.navigate("CoinRegistroScreen")
+
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.AddCircle,
+                            contentDescription = "AGREGAR",
+                            modifier = Modifier.size(30.dp)
+                        )
+
+                    }
+                }
+            )
+        }
+
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .fillMaxWidth()
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                items(state.coins) { coin ->
+                    CoinItem(coin = coin, {})
+                }
+            }
+
+            if (state.isLoading)
+                CircularProgressIndicator()
+        }
+    }
+}
