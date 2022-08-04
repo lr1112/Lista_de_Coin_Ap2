225ab254834b7d43077796089eb1a5cfad7c39f8
lr1112 <luis_rosario_0324@hotmail.com>
2022-08-04 10:44:27 -0400

    agregando el nombre de nuestra app al manifest y la etiqueta AndroidEntryPonit al mainActivity

--- app/src/main/java/edu/ucne/segundo_parcial_api/MainActivity.kt
+++ app/src/main/java/edu/ucne/segundo_parcial_api/MainActivity.kt
@@ -10,8 +10,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.segundo_parcial_api.ui.theme.Segundo_parcial_ApiTheme
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
